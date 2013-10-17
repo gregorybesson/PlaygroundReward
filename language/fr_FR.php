521c04c9c337538c50f326695f94ade2cb0a0cee
gonzalezemmanuel <emmanuel.gonzalez@adfab.fr>
2013-10-17 18:25:09 +0200

    PLAYGROUND-134 Playground - Setup 4 languages

--- language/fr_FR.php
+++ language/fr_FR.php
@@ -1,6 +1,62 @@
 <?php
 return array(
-	'bronze' 		=> 'bronze',
-	'silver'		=> 'argent',
-	'gold'			=> 'or',
+	
+	// Paginator
+    'First' 			=> 'Première',
+    'Previous' 			=> 'Précédente',
+    'Next' 				=> 'Suivante',
+    'Last' 				=> 'Dernière',
+    
+	// Admin reward List
+	'Add new Reward'	=> 'Créer une nouvelle récompense',
+	'Edit'				=> 'Editer',
+	'Delete' 			=> 'Supprimer',
+	'Id'				=> 'Id',
+	
+	// Admin reward Page
+	'Edit reward'		=> 'Editer une récompense',
+	'Title'				=> 'Titre',
+	'bronze' 			=> 'Bronze',
+	'silver'			=> 'Argent',
+	'gold'				=> 'Or',
+	'Yes'				=> 'Oui',
+	'No'				=> 'Non',
+	'Image'				=> 'Image',
+	'Active'			=> 'Actif',
+	'Reward type'		=> 'Type de recompense',
+	'Reward category'	=> 'Catégorie de récompense',
+	'Badge'				=> 'Badge',
+	'Trophy'			=> 'Trophée',
+	'Points'			=> 'Points',
+	'Limit occurrences'	=> 'Nombre d\'occurences',
+	'Manage rules'		=> 'Gérer les règles',
+	'Reward created'	=> 'Récompense créée',
+		
+	// Admin rules list
+	'Rules list'		=> 'Liste des règles',
+	'Rule'				=> 'Règle',
+	'Actions'			=> 'Actions',
+	'Add new rule'		=> 'Ajouter une règle',
+	'Id'				=> 'Id',
+		
+	//Admin rule page
+	'Completion type' 		=> 'S\'applique à',
+	'Any of these stories' 	=> 'l\'une des stories',
+	'All of these stories' 	=> 'Toutes les stories',
+	'Select a story'		=> 'Sélectionner une story',
+	'Conditions'			=> 'Conditions',
+	'Add a condition'		=> 'Ajouter une condition',
+	'Less than'				=> 'Moins de',
+	'Equals'				=> 'Egale à',
+	'More than'				=> 'Plus de',
+	'Count type'			=> 'Formule',
+	'Count'					=> 'Nombre',
+	
+	//Admin rule condition
+	'Object'			=> 'Objet',
+	'Attribute'			=> 'Attribut',
+	'Comparison'		=> 'Comparaison',
+	'Value'				=> 'Valeur',
+	'Comparison ?'		=> 'Comparaison ?',
+	'Delete'			=> 'Supprimer',
 );
