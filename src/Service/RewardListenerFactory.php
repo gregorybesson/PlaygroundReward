e827fe806b9cbf384d05d44861b72d484f751a6b
Greg Besson <bessong@gmail.com>
2019-04-02 14:49:11 +0200

    fix php7.1 error

--- src/Service/RewardListenerFactory.php
+++ src/Service/RewardListenerFactory.php
@@ -7,7 +7,7 @@ use Interop\Container\ContainerInterface;
 
 class RewardListenerFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $container, $requestedName, $options = null)
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $service = new RewardListener($container);
 
